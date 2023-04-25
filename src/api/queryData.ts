809054325c25c249868ef7fd981e180f81863ec2
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2023-04-25 12:48:42 +0300

    test

--- src/api/queryData.ts
+++ src/api/queryData.ts
@@ -1,5 +1,7 @@
 export const GITHUB_GRAPHQL_ENDPOINT = 'https://api.github.com/graphql';
 
+console.log(process.env);
+
 export const headers = {
   'Content-Type': 'application/json',
   Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
