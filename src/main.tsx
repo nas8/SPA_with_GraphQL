12e6f1a8920f372367818ac8da28d5285f000eea
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2023-04-25 11:53:15 +0300

    Added dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
+require('dotenv').config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
